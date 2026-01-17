6e8f011d9fff1cbae12ba79f0030ae951108cff2
Rohaansandhu <rohaan.sandhu@gmail.com>
2026-01-17 15:18:43 -0800

    Remove useless import

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,7 +1,6 @@
 import { Container, } from '@chakra-ui/react'
 import { Hero } from '../components/Landing/Hero.tsx'
 import { LegislatorPreview } from '../components/Legislators/LegislatorPreview.tsx'
-import { Features } from '../components/Landing/Features.tsx'
 import { FeaturedBills } from '../components/Bills/FeaturedBills.tsx'
 
 export default function Landing() {
